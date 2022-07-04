698c7b0645c24048c7ca075941023029c48b196e
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-04 13:57:02 +0900

    Articleモデルを介して、ビューには投稿された情報が表示されるようコントローラーを修正

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
+// 以下でリレーションを定義
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Article extends Model 
 {
-    //
+    public function user(): BelongsTo {
+        return $this->belongsTo('App\User');
+    }
 }
