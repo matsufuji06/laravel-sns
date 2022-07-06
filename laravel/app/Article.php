7042f523cfe80eee2542bf1d1df2816830caf635
matsufuji06 <n.matsufuji06@gmail.com>
2022-07-06 10:13:33 +0900

    fillableを利用して処理方法を修正

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -10,6 +10,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model 
 {
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     public function user(): BelongsTo {
         return $this->belongsTo('App\User');
     }
